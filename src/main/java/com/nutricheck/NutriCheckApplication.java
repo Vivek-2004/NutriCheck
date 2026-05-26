eee4ac30bd75e2c16e3e63619f17b49413f5c2a6
Vivek Ghosh <ghoshvivek004@gmail.com>
2026-05-27 02:42:59 +0530

    Fix project setup

--- src/main/java/com/nutricheck/NutriCheckApplication.java
+++ src/main/java/com/nutricheck/NutriCheckApplication.java
@@ -1,20 +1,11 @@
 package com.nutricheck;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import io.github.cdimascio.dotenv.DotenvEntry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NutriCheckApplication {
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.configure().load();
-		for (DotenvEntry i : dotenv.entries()) {
-			String key = i.getKey();
-			String value = i.getValue();
-			System.setProperty(key, value);
-		}
-
 		SpringApplication.run(NutriCheckApplication.class, args);
 	}
 }
